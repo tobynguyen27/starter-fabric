2c2dab41563251acb249974f207b1574ce23b3e4
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2026-01-05 20:40:33 +0700

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,142 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.fabric.loom)
+    alias(libs.plugins.spotless)
+    idea
+    `maven-publish`
+}
+
+val mod_version: String by project
+val mod_id: String by project
+val maven_group: String by project
+
+version = mod_version
+group = maven_group
+base.archivesName = mod_id
+
+repositories {
+    val repositories =
+        setOf(
+            "https://maven.parchmentmc.org", // Parchment
+            "https://maven.terraformersmc.com/", // ModMenu
+            "https://api.modrinth.com/maven/", // LazyDFU
+        )
+
+    repositories.forEach { maven { url = uri(it) } }
+    mavenCentral()
+}
+
+dependencies {
+    minecraft(libs.minecraft)
+    mappings(
+        loom.layered {
+            officialMojangMappings()
+            parchment("org.parchmentmc.data:parchment-1.18.2:2022.11.06@zip")
+        }
+    )
+
+    modImplementation(libs.fabric.loader)
+    modImplementation(libs.fabric.api)
+    modImplementation(libs.fabric.kotlin)
+
+    modLocalRuntime(libs.modmenu) { exclude(group = "net.fabricmc") }
+    modLocalRuntime(libs.lazydfu)
+}
+
+loom {
+    runs {
+        named("server") { runDir("run/server") }
+
+        named("client") { programArgs(setOf("--username", "Player")) }
+    }
+}
+
+tasks.withType<ProcessResources>().configureEach {
+    val mod_name: String by project
+    val mod_id: String by project
+    val mod_license: String by project
+    val mod_version: String by project
+    val mod_description: String by project
+
+    val replaceProperties =
+        hashMapOf(
+            "loader_version" to libs.versions.fabric.loader.get(),
+            "minecraft_version" to libs.versions.minecraft.get(),
+            "mod_id" to mod_id,
+            "mod_name" to mod_name,
+            "mod_license" to mod_license,
+            "mod_version" to mod_version,
+            "mod_description" to mod_description,
+        )
+
+    inputs.properties(replaceProperties)
+
+    filesMatching(setOf("fabric.mod.json")) { expand(replaceProperties) }
+}
+
+tasks.withType<JavaCompile>().configureEach { options.encoding = "UTF-8" }
+
+java {
+    toolchain { languageVersion = JavaLanguageVersion.of(17) }
+
+    withSourcesJar()
+}
+
+tasks.named<Jar>("jar") {
+    inputs.property("archivesName", project.base.archivesName)
+
+    from("LICENSE") { rename { "${it}_${inputs.properties["archivesName"]}" } }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = mod_id
+            version = project.version.toString()
+
+            from(components["java"])
+        }
+    }
+
+    repositories {}
+}
+
+spotless {
+    encoding("UTF-8")
+
+    kotlin {
+        ktfmt().kotlinlangStyle()
+        endWithNewline()
+        toggleOffOn()
+    }
+
+    kotlinGradle {
+        target("*.gradle.kts")
+        ktfmt().kotlinlangStyle()
+    }
+
+    java {
+        importOrder()
+        removeUnusedImports()
+        palantirJavaFormat()
+    }
+
+    json {
+        target("src/*/resources/**/*.json")
+        targetExclude("src/generated/resources/**")
+
+        biome("2.3.11")
+            .downloadDir(File(rootDir, ".gradle/biome").absolutePath)
+            .configPath(File(rootDir, "spotless/biome.json").absolutePath)
+
+        endWithNewline()
+    }
+}
+
+idea {
+    module {
+        isDownloadSources = true
+        isDownloadJavadoc = true
+    }
+}
---
+++ src/main/kotlin/com/example/examplemod/ExampleModClient.kt
@@ -0,0 +1,9 @@
+package com.example.examplemod
+
+import net.fabricmc.api.ClientModInitializer
+
+object ExampleModClient: ClientModInitializer {
+    override fun onInitializeClient() {
+        ExampleMod.LOGGER.info("Hello from client side")
+    }
+}
